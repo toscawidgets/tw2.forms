ab21b2bc331d72e5404950a8f157a39bfc3a5452
percious <none@none>
2009-07-06 23:12:04 -0600

    fixed tests to work with TW2, not TW

--- tests/base.py
+++ tests/base.py
@@ -3,9 +3,11 @@ from copy import copy
 from difflib import unified_diff
 from cStringIO import StringIO
 from cgi import FieldStorage
+from tw2.core.middleware import make_middleware
 
 #try:
 import xml.etree.ElementTree as etree
+from xml.parsers.expat import ExpatError
 #except ImportError:
 #    import cElementTree as etree
 
@@ -31,18 +33,29 @@ def remove_namespace(doc):
 
 def replace_escape_chars(needle):
     needle = needle.replace('&nbsp;', ' ')
+    needle = needle.replace(u'\xa0', ' ')
     return needle
 
 def fix_xml(needle):
     needle = replace_escape_chars(needle)
-    needle_node = etree.fromstring(needle)
+    try:
+        needle_node = etree.fromstring(needle)
+    except ExpatError:
+        raise ExpatError('Could not parse %s into xml.'%needle) 
     needle_node = remove_whitespace_nodes(needle_node)
     remove_namespace(needle_node)
     needle_s = etree.tostring(needle_node)
     return needle_s
 
 def in_xml(needle, haystack):
-    needle_s, haystack_s = map(fix_xml, (needle, haystack))
+    try:
+        needle_s = fix_xml(needle)
+    except ExpatError:
+        raise ExpatError('Could not parse needle: %s into xml.'%needle)
+    try:
+        haystack_s = fix_xml(haystack)
+    except ExpatError:
+        raise ExpatError('Could not parse haystack: %s into xml.'%haystack)
     return needle_s in haystack_s
 
 def eq_xml(needle, haystack):
@@ -54,3 +67,45 @@ def assert_in_xml(needle, haystack):
 
 def assert_eq_xml(needle, haystack):
     assert eq_xml(needle, haystack), "%s does not equal %s"%(needle, haystack)
+
+    
+import tw2.core as twc
+
+def request_local_tst():
+#    if _request_id is None:
+#        raise KeyError('must be in a request')
+    try:
+        return _request_local[_request_id]
+    except KeyError:
+        rl_data = {}
+        _request_local[_request_id] = rl_data
+        return rl_data
+
+import tw2.core.core
+tw2.core.core.request_local = request_local_tst
+from tw2.core.core import request_local
+
+_request_local=None
+_request_id=None
+
+class WidgetTest(object):
+    
+    template_engine = 'string'
+    params_as_vars = True
+    
+    def request(self, requestid):
+        global _request_id
+        _request_id = requestid
+        rl = request_local()
+        rl.clear()
+        rl['middleware'] = self.mw
+        return request_local_tst()
+
+    def setup(self):
+        global _request_id, _request_local
+        _request_local = {}
+        _request_id = None
+        self.mw = make_middleware(None, default_engine=self.template_engine)
+        return self.request(1)
+
+    
--- tests/test_widgets.py
+++ tests/test_widgets.py
@@ -1,39 +1,41 @@
-from tw.core.testutil import WidgetTestCase
-from tw.forms import *
-from base import assert_in_xml
+from tw2.forms.widgets import *
+from base import assert_in_xml, WidgetTest
+from nose.tools import raises
 
-class TestFormField(WidgetTestCase):
+class TestFormField(WidgetTest):
     # place your widget at the TestWidget attribute
     TestWidget = FormField
-    # Initilization args. go here 
-    widget_kw = {'name':'input'}
 
+    @raises(AttributeError)
     def test_display(self):
         # Asserts 'foo' and 'test' (the test widget's id) appear in rendered 
         # string when 'foo' is passed as value to render
         r = self.TestWidget().display()
         assert r == None, r
 
-class TestInputField(WidgetTestCase):
+class TestInputField(WidgetTest):
     
     TestWidget = InputField
     
     def test_display(self):
         # Asserts 'foo' and 'test' (the test widget's id) appear in rendered 
         # string when 'foo' is passed as value to render
-        r = self.TestWidget().display()
-        assert_in_xml('<input class="inputfield" value="" />', r)
+        r = self.TestWidget(type='foo').display()
+        #okay, inputs are flipping weird, go a head, try and uncomment this line below
+        #assert_in_xml('<input type="foo" />', r)
+        
+        assert '<input type="foo">' in r, r
         
     def test_display_override_css_class(self):
         class MyInputField(InputField):
             css_class = 'something'
-        r = MyInputField().display()
-        assert_in_xml('<input class="something" value="" />', r)
+            
+        r = MyInputField(type='foo').display()
+        assert '<input class="something" type="foo">' in  r, r 
+    
         
     def test_display_override_value(self):
-        r = self.TestWidget().display(value="something")
-        assert_in_xml('<input class="inputfield" value="something" />', r)
-        
-    
-    
+        r = self.TestWidget(type='foo').display(value="something")
+        assert '<input type="foo" value="something">' in r, r
+
 
