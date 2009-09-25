bec41f954b45d75768b1a546388fe29c4574a444
percious <none@none>
2009-09-25 16:43:31 -0600

    tagging for release.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='tw2.forms',
-    version='2.0.0-alpha1',
+    version='2.0a1',
     description='',
     author='Paul Johnston, Alberto Valverde & contributors',
     author_email='paj@pajhome.org.uk',
